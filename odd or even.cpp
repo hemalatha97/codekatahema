daf6376cf966c01e61a44746667a7ed30bf5f96d
hemalatha97 <36564357+hemalatha97@users.noreply.github.com>
2018-02-17 17:29:39 +0530

    Create odd or even.cpp

---
+++ odd or even.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{	
+        int num; 
+        cout<<"Enter Number : ";
+        cin>>num;
+        if(num>0)
+        {
+                cout<<"\n Number is odd\n";
+        }
+        else 
+        {
+                cout<<"\n Number is even \n";
+        }
+        
+        return 0;
+}
