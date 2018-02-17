eb63b2644d69b517e4a685e94320756483df2e8f
hemalatha97 <36564357+hemalatha97@users.noreply.github.com>
2018-02-17 18:02:09 +0530

    Create large.cpp

---
+++ large.cpp
@@ -0,0 +1,26 @@
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{	
+    
+        int a,b,c; a=1,b=2,c=3;
+        
+        if(a>b&&a>c)
+        {
+                cout<<" a laregest number\n";
+        }
+        else if(b>c) 
+        {
+                cout<<"b is an largest\n";
+        }
+        else
+        {
+            cout<<" c is largest";
+        }
+        
+        return 0;
+}
+
