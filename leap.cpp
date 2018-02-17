4fb5939cb2fecc3ce35fbc67da661d6f553c9e89
hemalatha97 <36564357+hemalatha97@users.noreply.github.com>
2018-02-17 17:53:54 +0530

    Create leap.cpp

---
+++ leap.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{	
+    
+        int num; 
+        cout<<"Enter a number : ";
+        cin>>num;
+        if(num%4==0)
+        {
+                cout<<" it is an leap year\n";
+        }
+        else 
+        {
+                cout<<"its not an  leap year\n";
+        }
+        
+        return 0;
+}
