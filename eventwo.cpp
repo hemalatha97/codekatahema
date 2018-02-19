f6618a5020f0e9e12df551681a4a5333afa5c5d4
hemalatha97 <36564357+hemalatha97@users.noreply.github.com>
2018-02-19 17:39:15 +0530

    Create eventwo.cpp

---
+++ eventwo.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+ 
+int main()
+{	
+        int num, a, b; 
+        cout<<"Enter Number : ";
+        cin>>a>>b;
+        while(a<=b)
+        {
+        if(a%2 ==0)
+        
+            
+       cout<<a;
+       a++;
+        }
+        return 0;
+}
