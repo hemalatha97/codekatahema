7ba2b0b32f61962651555dd67783cb84e6b63834
hemalatha97 <36564357+hemalatha97@users.noreply.github.com>
2018-02-20 17:17:03 +0530

    Create maximum.cpp

---
+++ maximum.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int j,l;
+    int arr[100];
+    cout << "Enter number of elements "<<endl;
+    cin >> l;
+    for(j = 0; j < l; ++j)
+    {
+       cout << "Enter Number"<<endl;
+       cin >> arr[j];
+    }
+    for(j = 1;j < l; ++j)
+    {
+       if(arr[0] < arr[j])
+           arr[0] = arr[j];
+    }
+    cout << "maximum element = " << arr[0];
+
+    return 0;
+}
