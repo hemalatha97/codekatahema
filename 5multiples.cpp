1a3129c2bc11a7880185edb485070a6ce9f0b331
hemalatha97 <36564357+hemalatha97@users.noreply.github.com>
2018-02-20 16:40:14 +0530

    Create 5multiples.cpp

---
+++ 5multiples.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+ 
+    int n;
+cout << "Enter a number"<<endl;
+    cin >> n;
+for (int i = 1; i <= 5; ++i) {
+        cout <<n * i << endl;
+    }
+    
+    return 0;
+}
