1e78102c53e950ffd4c4e06872610c0603ee75b6
hemalatha97 <36564357+hemalatha97@users.noreply.github.com>
2018-02-21 17:57:11 +0530

    Create countwords.cpp

---
+++ countwords.cpp
@@ -0,0 +1,18 @@
+int main()
+{
+    char a[100];
+    int i,count=1;
+    
+    cout<<"Enter a string:";
+    gets(a);
+    
+    for(i=0;a[i]!='\0';++i)
+    {
+        if(a[i]==' ')
+            count++;
+    }
+    
+    cout<<"\nThere are "<<count<<" words in the given string";
+ 
+    return 0;
+}
