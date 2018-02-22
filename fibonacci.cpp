4679906243b6f61aab83938162a45c94a469326c
hemalatha97 <36564357+hemalatha97@users.noreply.github.com>
2018-02-22 17:32:29 +0530

    Update fibonacci.cpp

--- fibonacci.cpp
+++ fibonacci.cpp
@@ -4,14 +4,14 @@ using namespace std;
 
 int main()
 {
-int i,no, first=0, second=1, next;
+int j,no, first=0, second=1, next;
 
 first=0;
 second=1;
 cout<<"Enter nubmer of terms for Series: ";
 cin>>no;
-cout<<"Fibonacci series are: \n";
-for(i=0; i<no; i++)
+cout<<"Fibonacci series are";
+for(j=0; j<no; j++)
 {
 cout<<"\n"<<first;
 next = first + second;
