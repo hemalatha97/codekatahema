6e4f721a68a67e57cee4b792784d7af8e010cc35
hemalatha97 <36564357+hemalatha97@users.noreply.github.com>
2018-02-22 17:40:37 +0530

    Create numberofspace.cpp

---
+++ numberofspace.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+
+       {
+
+              char str[50];
+              int i,count=0;
+             
+
+              printf("\nEnter any string : ");
+              gets(str);
+              
+              for(i=0;str[i];i++)
+                     if(str[i] == ' ')
+                           count++;
+              
+              printf("\nTotal blank spaces : %d",count);
+
+             return 0;
+    }
