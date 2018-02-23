e4a015d5d9ba14cc5517a702a2fdbf0bc3cd2f15
hemalatha97 <36564357+hemalatha97@users.noreply.github.com>
2018-02-23 17:35:27 +0530

    Create sprint.cpp

---
+++ sprint.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	string str1;
+	cout<<"enter the string"<<endl;
+	cin>>str1;
+	int j;
+	cout<<"enter the number"<<endl;
+	cin>>j;
+	for(int i=0;i<j;i++)
+	{
+	cout<<str1<<endl;	
+	}
+	return 0;
+}
