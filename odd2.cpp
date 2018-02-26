f5dacf33bfa0761b6ed189eb619bf8c48ac0896e
hemalatha97 <36564357+hemalatha97@users.noreply.github.com>
2018-02-26 17:46:55 +0530

    Create odd2.cpp

---
+++ odd2.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+int main()
+{
+   int n;
+   cout<<"enter the number";
+   cin>>n;
+   if(n%2==0)
+   {
+       cout<<"number is odd";
+       }
+   else
+   {
+       cout<<"number is even";
+   }
+   return 0;
+}
