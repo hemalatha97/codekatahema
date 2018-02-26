08a4a1f4d17c840dabed2d0de8af0dfd58a955ba
hemalatha97 <36564357+hemalatha97@users.noreply.github.com>
2018-02-26 17:34:18 +0530

    Create among10.cpp

---
+++ among10.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+int a[10];
+    int i;
+    int greatest;
+    cout<<"Enter ten values:";
+    
+    for (i = 0; i < 10; i++) {
+    cin >> a[i];
+    }
+    
+    greatest = a[0];
+    for (i = 0; i < 10; i++) {
+if (a[i] > greatest) {
+greatest = a[i];
+    }
+    }
+    cout<<"Greatest of ten numbers is %d", greatest;
+    return 0;
+  }
