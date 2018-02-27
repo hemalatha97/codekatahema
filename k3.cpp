5554a7b719762e1cf473064ef41ce79e267dac43
hemalatha97 <36564357+hemalatha97@users.noreply.github.com>
2018-02-27 17:53:12 +0530

    Rename k1.cpp to k3.cpp

---
+++ k3.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+
+char s[10]="laptop";
+int n=3,i,num;
+cout<<"enter the n";
+cin>>num;
+for(i=0;i<n;i++)
+{
+cout<<s;
+}
+return 0;
+}
