7b072607f67a903f79ec79e11989b59dbd6a178f
hemalatha97 <36564357+hemalatha97@users.noreply.github.com>
2018-03-01 13:36:04 +0530

    Create multi13.cpp

---
+++ multi13.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+   {
+
+	int h;
+
+	cout<<"enter the number:";
+
+	cin>>h;
+
+	if(h%13==0)
+
+	{
+
+		cout<< "yes";
+
+	}
+
+	else
+
+	{
+
+		cout<< "no";
+	}
+
+	return 0;
+
+
+}
+}
+
+
+
