8d113b20031f7ff8d34781b1a923e3f8340c0557
hemalatha97 <36564357+hemalatha97@users.noreply.github.com>
2018-03-01 13:57:26 +0530

    Create range10.cpp

---
+++ range10.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int n,i;
+   cout<<"enter the number";
+   cin>>n;
+
+       for(i=1;i<=10;i++)
+       {
+         if(i==n)
+		{
+			cout<<"yes";
+		} 
+		else
+		{
+		    cout<<"no";
+		}
+       }
+      
+
+       
+       return 0;
+}
+
+      
+
+
+
