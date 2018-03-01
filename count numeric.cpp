ac78a23a61f47b74d879a0caf24db5ae9d5382b6
hemalatha97 <36564357+hemalatha97@users.noreply.github.com>
2018-03-01 18:11:26 +0530

    Create count numeric.cpp

---
+++ count numeric.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+     int num;
+   int count=0;
+ 
+   cout<<"Enter a number \n";
+  cin>> num;
+
+   while(num > 0) {
+num=num/10;
+      count++;
+  }
+ 
+  cout<<count;
+ 
+  return 0;
+}
