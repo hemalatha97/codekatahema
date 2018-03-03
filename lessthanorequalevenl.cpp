7cee1349abf701c0bced2ceae6f203d30c3c9ffe
hemalatha97 <36564357+hemalatha97@users.noreply.github.com>
2018-03-03 16:29:36 +0530

    Create lessthanorequalevenl.cpp

---
+++ lessthanorequalevenl.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int i;
+  cout<<"Enter the number"<<endl;
+  cin>>i;
+  if(i%2==0)
+    {
+     cout<<"The near even num is"<<i;
+    }
+  else
+    {
+      cout<<"The near even num is"<<i-1;
+   }
+  return 0;
+}
+
+  
+
+
+
+
+
