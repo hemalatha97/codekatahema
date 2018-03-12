5cb7c80675eddadbee2b65885c88ab6b52790421
hemalatha97 <36564357+hemalatha97@users.noreply.github.com>
2018-03-12 17:51:02 +0530

    Create pnr.cpp

---
+++ pnr.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using namespace std;
+
+
+
+ int main()
+{
+	 float j,h,s,z;
+    cout<<"amount";
+    cin>>j;
+    cout<<"time";
+    cin>>h;
+    cout<<"rate";
+    cin>>s;
+    z=(j*h*s)/100;
+    cout<<z;
+
+
+
+}
