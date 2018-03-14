076139f08dcc85b58edbd97fa5ce499ae025057c
hemalatha97 <36564357+hemalatha97@users.noreply.github.com>
2018-03-13 21:11:43 -0700

    Create alphabet1.cpp

---
+++ alphabet1.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+     int number,j;
+
+    char a=65;
+
+   cin>>number;
+
+   for(j=1;j<number;j++)
+
+   {
+
+    a++;
+
+} 
+
+cout<<a;
+}
