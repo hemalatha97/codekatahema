e421985945b30a7e3ed798951837d4ed23074c90
hemalatha97 <36564357+hemalatha97@users.noreply.github.com>
2018-03-15 01:10:46 -0700

    Create knatural.cpp

---
+++ knatural.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int K,i,sum=0;
+
+    cout<<"num:";
+
+    cin>>K;
+
+    for(i=0;i<=K;i++)
+
+    {
+
+        sum=sum+i;
+
+    }cout<<sum;
+
+    return 0;
+}
