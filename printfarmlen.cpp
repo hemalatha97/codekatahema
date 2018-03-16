f02932ea433dfc63491c5eb4134c6852f94d55e9
hemalatha97 <36564357+hemalatha97@users.noreply.github.com>
2018-03-15 20:52:19 -0700

    Create printfarmlen.cpp

---
+++ printfarmlen.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    float length,breath,areaval;
+
+   cout<<"length VALUE:";
+
+   cin>>length;
+
+   cout<<"breadth VALUE:";
+
+   cin>>breath;
+
+   areaval=length*breath;
+
+   cout<<areaval;
+
+   return 0;
+}
