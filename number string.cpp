85045056c31b2984128a5498e946071bdabd4ca5
hemalatha97 <36564357+hemalatha97@users.noreply.github.com>
2018-03-19 09:47:48 +0530

    Create number string.cpp

---
+++ number string.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+
+{
+    
+char s[10];
+
+ int n,i,count=0;
+
+ cout<<"enter a string";
+
+ cin>>s;
+
+ n=strlen(s);
+
+ for(i=0;i>n;i++)
+
+ {
+
+   if(s[i]>=0 || s[i]<=9)
+
+   {
+
+    count++;
+
+    cout<<icout<<count;
+    return 0;
+
+   }
+}
+
+
