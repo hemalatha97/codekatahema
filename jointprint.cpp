69714363acf618caa5ba0eb2da70f43373313e02
hemalatha97 <36564357+hemalatha97@users.noreply.github.com>
2018-03-21 16:16:15 +0530

    Create jointprint.cpp

---
+++ jointprint.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+
+#include<math.h>
+
+using namespace std;
+
+int main()
+
+{
+int c,d;
+
+  cout<<"c:";
+
+  cin>>c;
+
+  cout<<"d:";
+
+  cin>>d;
+
+  cout<<c<<d;
+  return 0;
+}
