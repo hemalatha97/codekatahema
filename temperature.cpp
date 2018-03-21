b9dc93c83eb6d1cd21a32a102b83b1ec4e875bb4
hemalatha97 <36564357+hemalatha97@users.noreply.github.com>
2018-03-21 16:03:54 +0530

    Create temperature.cpp

---
+++ temperature.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+
+#include<math.h>
+
+using namespace std;
+
+int main()
+
+{
+
+    int tempe,celsius=0;
+
+    cout<<"temperature";
+
+    cin>>tempe;
+
+    celsius=tempe+273;
+
+    cout<<celsius;
+
+}
