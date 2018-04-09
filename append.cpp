83ca38ed2ffd090d61153637da00b00df55838b0
hemalatha97 <36564357+hemalatha97@users.noreply.github.com>
2018-04-09 13:44:41 +0530

    Create append.cpp

---
+++ append.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <string>
+using namespace std;
+int main()
+{
+string s;
+char y='.';
+cout<<"INPUT"<<endl;
+cin>>s;
+s=s+y;
+cout<<"OUTPUT"<<endl;
+cout<<s;
+return 0;
+}
