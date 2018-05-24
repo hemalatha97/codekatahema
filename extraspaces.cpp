3232fc0c220a9cc6821b0beee434a714becdd33a
hemalatha97 <36564357+hemalatha97@users.noreply.github.com>
2018-05-24 12:34:43 +0530

    Create extraspaces.cpp

---
+++ extraspaces.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <string.h>
+using namespace std;
+
+int main()
+{
+
+  int i,j=0;
+    char str[30];
+    cout<<"Enter a String:";
+    gets(str);
+    
+    for(i=0;str[i]!='\0';++i)
+    {
+    if(str[i]!=' ')
+    str[j++]=str[i];
+    }
+    
+    str[j]='\0';
+    
+    cout<<"String After Removing Spaces:"<<str;
+    return 0;
+}
